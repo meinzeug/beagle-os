5e7dc1951ac84d5ff12cb34abe5ad243a01cc377
Dennis Wicht <dennis.wicht@web.de>
2026-05-23 19:54:38 +0200

    thinclient: guard stream clean-exit relaunch loops

--- thin-client-assistant/runtime/session_launcher.sh
+++ thin-client-assistant/runtime/session_launcher.sh
@@ -57,13 +57,24 @@ beagle_launch_beagle_stream_client_session() {
   local launcher_status=127
   local relaunch_enabled="${PVE_THIN_CLIENT_BEAGLE_STREAM_CLIENT_RELAUNCH:-1}"
   local relaunch_delay="${PVE_THIN_CLIENT_BEAGLE_STREAM_CLIENT_RELAUNCH_DELAY_SECONDS:-3}"
+  local relaunch_max_delay="${PVE_THIN_CLIENT_BEAGLE_STREAM_CLIENT_RELAUNCH_MAX_DELAY_SECONDS:-30}"
+  local quick_exit_window="${PVE_THIN_CLIENT_BEAGLE_STREAM_CLIENT_QUICK_EXIT_WINDOW_SECONDS:-45}"
+  local quick_exit_limit="${PVE_THIN_CLIENT_BEAGLE_STREAM_CLIENT_QUICK_EXIT_LIMIT:-6}"
+  local quick_exit_count=0
+  local launch_started_at=0
+  local launch_ended_at=0
+  local launch_runtime=0
+  local sleep_for=0
 
   while true; do
+    launch_started_at="$(date +%s)"
     if bash "$SCRIPT_DIR/launch-beagle-stream-client.sh"; then
       launcher_status=0
     else
       launcher_status=$?
     fi
+    launch_ended_at="$(date +%s)"
+    launch_runtime=$((launch_ended_at - launch_started_at))
 
     if [[ "$relaunch_enabled" != "1" ]]; then
       return "$launcher_status"
@@ -72,11 +83,27 @@ beagle_launch_beagle_stream_client_session() {
       return 0
     fi
 
+    sleep_for="$relaunch_delay"
     if [[ "$launcher_status" -eq 0 ]]; then
-      beagle_log_event "launch-session.beagle-stream-client" "state=clean-exit relaunch=1"
+      if (( launch_runtime < quick_exit_window )); then
+        quick_exit_count=$((quick_exit_count + 1))
+        sleep_for=$((relaunch_delay * (quick_exit_count + 1)))
+        if (( sleep_for > relaunch_max_delay )); then
+          sleep_for="$relaunch_max_delay"
+        fi
+        beagle_log_event "launch-session.beagle-stream-client" "state=clean-exit relaunch=1 runtime=${launch_runtime}s quick_exits=${quick_exit_count} sleep=${sleep_for}s"
+        if (( quick_exit_count >= quick_exit_limit )); then
+          beagle_log_event "launch-session.beagle-stream-client" "state=clean-exit-loop status=0 runtime=${launch_runtime}s quick_exits=${quick_exit_count} action=abort-relaunch"
+          return 75
+        fi
+      else
+        quick_exit_count=0
+        beagle_log_event "launch-session.beagle-stream-client" "state=clean-exit relaunch=1 runtime=${launch_runtime}s"
+      fi
     else
-      beagle_log_event "launch-session.beagle-stream-client" "state=failed status=${launcher_status} relaunch=1"
+      quick_exit_count=0
+      beagle_log_event "launch-session.beagle-stream-client" "state=failed status=${launcher_status} relaunch=1 runtime=${launch_runtime}s"
     fi
-    sleep "$relaunch_delay"
+    sleep "$sleep_for"
   done
 }
